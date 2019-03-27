bbd5db8b4a1e0ff745d68597d4ea9b89ed5f715e
masalyuk <masalyuk@gmail.com>
2019-03-27 23:55:16 +0300

    added abstact class denoiser

---
+++ denoise_algorithms/ADenoiser.py
@@ -0,0 +1,14 @@
+class ADenoieser:
+	"img - denoise image"
+
+	def __init__(self, img):
+		self.img = img
+
+	def set_parameters(self, params):
+		self.params = params
+
+	def disp_parameters(self):
+		pass
+
+	def denoise(self):
+		pass
